6fc02f7ecbd2ea5bab70182aa2357e5a07bd5bb3
mahirTheCoder <mahirchowdhury518@gmail.com>
2026-05-06 23:43:39 +0600

    extructure

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { BrowserRouter } from 'react-router'
+
+const App = () => {
+  return (
+    <>
+    <BrowserRouter>
+    
+    
+    </BrowserRouter>
+    
+    </>
+  )
+}
+
+export default App
